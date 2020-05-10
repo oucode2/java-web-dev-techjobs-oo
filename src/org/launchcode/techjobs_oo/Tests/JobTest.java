127cbba84276338628015eb6639098a5af02a24b
Kenan Kadic <ken.kadic@outlook.com>
2020-05-10 15:23:36 -0500

    new commit

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -18,20 +18,28 @@ public class JobTest {
     public void createCarObject() {
         test_jobOne = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         test_jobTwo = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+        test_jobThree = new Job("Product teste", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
     }
+
     @Test
     public void testSettingJobId() {
         assertFalse(test_jobOne.getId() == test_jobTwo.getId());
-        assertTrue(test_jobTwo.getId() == test_jobOne.getId()+1);
+        assertTrue(test_jobTwo.getId() == test_jobOne.getId() + 1);
     }
+
     @Test
     public void testJobConstructorSetAllFields() {
         assertTrue(test_jobOne instanceof Job);
-        assertEquals(1, test_jobOne.getId(),.001);
-        assertEquals("Product tester", test_jobOne.getEmployer());
-        //assertEquals("ACME", test_jobOne.getEmployer());
-        //assertEquals("Desert", test_jobOne.getLocation());
-        //assertEquals("Quality control", test_jobOne.getPositionType());
-        //assertEquals("Persistence", test_jobOne.getCoreCompetency());
+        assertEquals(1, test_jobOne.getId(), .001);
+        assertEquals("Product tester", test_jobOne.getName());
+        assertEquals("ACME", test_jobOne.getEmployer().getValue());
+        assertEquals("Desert", test_jobOne.getLocation().getValue());
+        assertEquals("Quality control", test_jobOne.getPositionType().getValue());
+        assertEquals("Persistence", test_jobOne.getCoreCompetency().getValue());
+    }
+
+    public void testJobsForEquality() {
+        assertFalse(test_jobOne, test_jobThree);
+        assertTrue(test_jobTwo.getId() == test_jobOne.getId() + 1);
     }
 }
