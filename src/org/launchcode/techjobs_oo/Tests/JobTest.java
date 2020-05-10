ef216a710a97233fa3148d1de6ea6aea92b03cdf
Kenan Kadic <ken.kadic@outlook.com>
2020-05-10 14:03:33 -0500

    another commit

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,4 +1,26 @@
 package org.launchcode.techjobs_oo.Tests;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import org.launchcode.techjobs_oo.Main;
+import org.launchcode.techjobs_oo.CoreCompetency;
+import org.launchcode.techjobs_oo.PositionType;
+import org.launchcode.techjobs_oo.Location;
+import org.launchcode.techjobs_oo.Employer;
+import org.launchcode.techjobs_oo.Job;
+
 
 public class JobTest {
+    Job test_jobOne;
+    Job test_jobTwo;
+
+    @Before
+    public void createCarObject() {
+        test_jobOne = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        test_jobTwo = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+    }
+    @Test
+    public void testSettingJobId() {
+        assertEquals(10, 10, .001);
+    }
 }
