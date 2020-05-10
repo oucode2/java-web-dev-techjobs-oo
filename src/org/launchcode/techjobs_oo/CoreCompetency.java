6147a6e28598de05ce9e5e69ee2fad8e19e4cb40
Kenan Kadic <ken.kadic@outlook.com>
2020-05-10 12:36:31 -0500

    fourth commit

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -7,14 +7,6 @@ public class CoreCompetency {
     private static int nextId = 1;
     private String value;
 
-    public String getValue() {
-        return value;
-    }
-
-    public int getId() {
-        return id;
-    }
-
     public void setValue(String value) {
         this.value = value;
     }
@@ -51,4 +43,11 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+    public String getValue() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
